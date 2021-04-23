4e126b6f98bb3cc6aea2a56361ca953db8d77ec3
KevinZiadeh <kevinziadeh@gmail.com>
2021-04-23 10:23:16 +0300

    Added email reminder that runs every 30 minutes

--- appointment/apps.py
+++ appointment/apps.py
@@ -3,3 +3,7 @@ from django.apps import AppConfig
 
 class AppointmentConfig(AppConfig):
     name = 'appointment'
+
+    def ready(self):
+        from . import email_scheduler
+        email_scheduler.start()
--- clinicX/settings.py
+++ clinicX/settings.py
@@ -135,3 +135,11 @@ CHAT_WS_SERVER_PORT = 5002
 CHAT_WS_SERVER_PROTOCOL = 'ws'
 
 os.environ["DJANGO_ALLOW_ASYNC_UNSAFE"] = "true"
+
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'eece430ehms@gmail.com'
+EMAIL_HOST_PASSWORD = 'eece430EHM$'
