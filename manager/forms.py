087f2898b9760827a581c2052884994f40c7c440
KevinZiadeh <kevinziadeh@gmail.com>
2021-04-26 21:48:17 +0300

    Added form validation for manager

--- manager/forms.py
+++ manager/forms.py
@@ -10,12 +10,12 @@ class ManagerForm(ModelForm):
         fields = ('role',)
 
 
-
 class UserForm(forms.ModelForm):
     password = forms.CharField(widget=forms.PasswordInput())
     email = forms.CharField(max_length=150, required=True)
     first_name = forms.CharField(max_length=150, required=True)
     last_name = forms.CharField(max_length=150, required=True)
+
     class Meta:
         model = User
         fields = ('username', 'first_name', 'last_name', 'email', 'password')
--- manager/models.py
+++ manager/models.py
@@ -3,6 +3,7 @@ from django.contrib.auth.models import AbstractUser, User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
+
 class Report(models.Model):
 	date = models.DateField()
 	totalPatient = models.IntegerField()
